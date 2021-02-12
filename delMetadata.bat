e80597187cb8702da66ede785b430419b38bb3f0
Siddhanth B N <56065640+pol905@users.noreply.github.com>
2021-02-12 16:21:56 +0530

    Update delMetadata.bat

--- delMetadata.bat
+++ delMetadata.bat
@@ -1,6 +1,6 @@
 @ECHO off
 
-FOR /F "tokens=2" %%c IN ('@CMDKEY /list ^| @FINDSTR space:space') do (@CMDKEY /Q /delete:%%c)
+FOR /F "tokens=2" %%c IN ('@CMDKEY /list ^| @FINDSTR space:space') do (@CMDKEY /delete:%%c)
 
 @RD /S /Q "C:\\Users\\%USERNAME%\\.fleek-ipfs"
 @RD /S /Q "C:\\Users\\%USERNAME%\\.fleek-space"
