575400936d46889a472212ef1ddb16353f472392
Siddhanth B N <56065640+pol905@users.noreply.github.com>
2021-02-12 12:06:05 +0530

    Initial Commit

---
+++ delMetadata.bat
@@ -0,0 +1,10 @@
+@ECHO off
+
+FOR /F "tokens=2" %%c IN ('@CMDKEY /list ^| @FINDSTR space:space') do (@CMDKEY /Q /delete:%%c)
+
+@RD /S /Q "C:\\Users\\%USERNAME%\\.fleek-ipfs"
+@RD /S /Q "C:\\Users\\%USERNAME%\\.fleek-space"
+@RD /S /Q "C:\\Users\\%USERNAME%\\.buckd"
+
+@ECHO Removed credentials and deleted space associated folders successfully
+
